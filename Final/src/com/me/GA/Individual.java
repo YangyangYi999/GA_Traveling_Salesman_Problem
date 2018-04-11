433738db4179e3f6c007db603f9c76bf0120e679
yiyangyang <yiyangyang@10.110.17.187>
2018-04-10 21:39:01 -0400

    yyy

---
+++ Final/src/com/me/GA/Individual.java
@@ -0,0 +1,23 @@
+package com.me.GA;
+
+public class Individual {
+	private boolean binaryCity[];
+	private int decimalCity[];
+
+	public boolean[] getBinaryCity() {
+		return binaryCity;
+	}
+
+	public void setBinaryCity(boolean[] binaryCity) {
+		this.binaryCity = binaryCity;
+	}
+
+	public int[] getDecimalCity() {
+		return decimalCity;
+	}
+
+	public void setDecimalCity(int[] decimalCity) {
+		this.decimalCity = decimalCity;
+	}
+
+}
---
+++ Final/src/com/me/GA/Population.java
@@ -0,0 +1,17 @@
+package com.me.GA;
+
+import java.util.ArrayList;
+
+public class Population {
+	private ArrayList <Individual> population = new ArrayList<Individual>();
+
+	public ArrayList<Individual> getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(ArrayList<Individual> population) {
+		this.population = population;
+	}
+	
+	
+}
